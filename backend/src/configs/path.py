00875291544705598bfceaa2abc59eda8e9b6822
tgd1115 <gdtan021115@gmail.com>
2025-04-24 01:28:25 +0800

    update main.py

--- backend/src/configs/path.py
+++ backend/src/configs/path.py
@@ -13,3 +13,14 @@ class PathConfig:
     FEATURES_DIR: str = f"{DATA_DIR}/features"
 
     MODELS_DIR: str = f"{BASE_DIR}/models"
+
+    @staticmethod
+    def createDirectories(self):
+        directories = [self.DATA_DIR, self.RAW_DATA_DIR, self.FEATURES_DIR, self.MODELS_DIR]
+
+        for directory in directories:
+            os.makedirs(directory, exist_ok=True)
+
+        print("Successfully created the directories.")
+
+    
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@ import uvicorn as uv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from configs.path import PathConfig
 from data.data_loader import DataLoader
 from errors.base_exception import BacktesterError
 from handlers.exception_handler import appExceptionHandler
@@ -26,24 +27,19 @@ def createApp() -> FastAPI:
     return app
 
 app = createApp()
-# dataLoader = DataLoader()
-# dataLoader.run(category="exchange-flows")
-
-# dataLoader.run(category="flow-indicator")
-
-# dataLoader.run(category="market-indicator")
-
-# dataLoader.run(category="network-indicator")
-
-# dataLoader.run(category="miner-flows")
-
-# dataLoader.run(category="market-data")
-
-# dataLoader.run(category="network-data")
-
 
 if __name__ == "__main__":
     uv.run(
         "main:app",
         reload=True
     )
+
+    # PathConfig.createDirectories(PathConfig)
+    # dataLoader = DataLoader()
+    # dataLoader.run(category="exchange-flows")
+    # dataLoader.run(category="flow-indicator")
+    # dataLoader.run(category="market-indicator")
+    # dataLoader.run(category="network-indicator")
+    # dataLoader.run(category="miner-flows")
+    # dataLoader.run(category="market-data")
+    # dataLoader.run(category="network-data")
