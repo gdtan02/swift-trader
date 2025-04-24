fae160aae400d3215497bc04c8429dd9d83b8a71
tgd1115 <gdtan021115@gmail.com>
2025-04-24 16:58:49 +0800

    Dependency injection for backtester service

--- backend/src/api/v1/routes/backtest.py
+++ backend/src/api/v1/routes/backtest.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
 
+from api.v1.routes.deps import getBacktesterService
+from services.backtester import BacktesterService
 from errors.base_exception import BacktesterError
 from schemas.base import ResponseModel
 from schemas.backtest import BacktestRequestModel, BacktestResponseModel
@@ -7,12 +9,12 @@ from schemas.backtest import BacktestRequestModel, BacktestResponseModel
 backtestRouter = APIRouter(prefix="/backtest", tags=["Backtester"])
 
 @backtestRouter.post("/simulate-trade", response_model=ResponseModel[BacktestResponseModel])
-async def simulateTrade(request: BacktestRequestModel):
+async def simulateTrade(request: BacktestRequestModel, backtestService: BacktesterService = Depends(getBacktesterService)):
 
-    if request:
-        print("Test.")
+    result: BacktestResponseModel = await backtestService.run(request)
 
     return ResponseModel(
         success=True,
-        data = BacktestResponseModel()
+        data = BacktestResponseModel(),
+        error = None
     )
