7e11b016c694a306eb1c6b1e071fde08fa44b47f
tgd1115 <gdtan021115@gmail.com>
2025-04-25 18:12:10 +0800

    refactor: code refactor and add retrainModel function

--- backend/src/strategies/position_sizer/hmm_position_sizer.py
+++ backend/src/strategies/position_sizer/hmm_position_sizer.py
@@ -14,43 +14,63 @@ from strategies.position_sizer.base_position_sizer import BasePositionSizer
 class HMMPositionSizer(BasePositionSizer):
     """Automatic adjust position sizing based on market regime."""
     
-    def __init__(self, backtestDate: str, loader: Optional[FeatureLoader] = None, lookBackPeriod: int = 30, tradeFees: float = 0.0006):
-        self.loader = loader if loader is not None else FeatureLoader(backtestDate=backtestDate)
-        self.backtestDate = backtestDate
-        self.lookBackPeriod: int = lookBackPeriod
-        self.model: Algorithm = HMMRegimeModel()
+    def __init__(self, backtestEndDate: str, data: Optional[pd.DataFrame] = None, loader: Optional[FeatureLoader] = None, lookBackPeriod: int = 30, tradeFees: float = 0.0006):
+
+        self.data = data
+        self.loader = loader if loader is not None else FeatureLoader(backtestDate=backtestEndDate)
+        self.backtestEndDate = backtestEndDate
+        
         self.selectedFeatures: List[str] = ["daily_return", "log_volatility"]
         self.features: List[Feature] = None
+        
         self.lookBackPeriod = lookBackPeriod
         self.tradeFees = tradeFees
-        self.df: pd.DataFrame = []
 
-        self._initializeData()
-        self._trainModel()
+        self.initializeStrategy()
+
+    def initializeStrategy(self, train: bool = False):
+
+        self.features = [
+            AdjustedReturnRate(tradeFees=self.tradeFees),
+            Volatility(windowSize=self.lookBackPeriod)
+        ]
+        dataWithFeatures = self.loader.loadFeatures(data = self.data, features=self.features)
+        dataWithFeatures.dropna(inplace=True)
+
+        dataWithFeatures.to_csv(f"{PathConfig.FEATURES_DIR}/hmm_features.csv")
+
+        self.data = dataWithFeatures
+
+        if self.data is None:
+            raise BacktesterError("strategy/data-not-loaded")
+    
+        self._initializeModel(self.data[self.selectedFeatures], train=train)
+        
+    def retrainModel(self, data: pd.DataFrame):
+        self.data = data
+        self.initializeStrategy(train=True)
 
     def calculatePositionSize(self, orderDate: str, availableCash: float):
 
         if not self.model.isFitted:
             raise BacktesterError("algorithm/model-not-fitted")
         
-        if self.df is None:
+        if self.data is None:
             raise BacktesterError("strategy/data-not-loaded")
         
-        self.df["regimes"] = self.model.predict(self.df)
-        print(self.df.groupby("regimes").mean())
+        if "regimes" not in self.data.columns:
+            raise BacktesterError("feature/missing-columns", details="Column name not found: regimes")
 
-        print("Order:", self.df.loc[orderDate])
-
-        if self.df.loc[orderDate]["regimes"] == 3:
+        if self.data.loc[orderDate]["regimes"] == 3:
             positionSize = 0.8
         
-        elif self.df.loc[orderDate]["regimes"] == 4:
+        elif self.data.loc[orderDate]["regimes"] == 4:
             positionSize = 0.6
         
-        elif self.df.loc[orderDate]["regimes"] == 2:
+        elif self.data.loc[orderDate]["regimes"] == 2:
             positionSize = 0.4
         
-        elif self.df.loc[orderDate]["regimes"] == 1:
+        elif self.data.loc[orderDate]["regimes"] == 1:
             positionSize = 0.2
         
         else:
@@ -58,27 +78,16 @@ class HMMPositionSizer(BasePositionSizer):
 
         return availableCash * positionSize
 
-    def _initializeData(self):
-        self.features = [
-            AdjustedReturnRate(tradeFees=self.tradeFees),
-            Volatility(windowSize=self.lookBackPeriod)
-        ]
-        self.df = self.loader.loadFeatures(features=self.features)[self.selectedFeatures]
-        print("Index", self.df.index)
-        self.df.dropna(inplace=True)
+    def _initializeModel(self, data, train=False):
 
-        self.df.to_csv(f"{PathConfig.FEATURES_DIR}/hmm_features.csv")
+        self.model: Algorithm = HMMRegimeModel()
 
-    def _trainModel(self):
-        if self.df is None:
-            raise BacktesterError("strategy/data-not-loaded")
-        
-        if self.model.isFitted:
-            return
-        
-        self.model.fit(X = self.df[:self.backtestDate], optimize=True)
+        if train or not self.model.isFitted:
+            self.model.fit(data.loc[:self.backtestEndDate][self.selectedFeatures], optimize=True)  # Train until the backtest end date
 
-    
-        
-        
-    
+        regimes = self.model.predict(data)  # Predict the regimes for all the features
+        data = data.copy()
+        data["regimes"] = regimes
+        print(data.groupby("regimes").mean())
+
+        self.data = data
